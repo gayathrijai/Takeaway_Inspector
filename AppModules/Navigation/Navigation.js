92bf84feab33d78e8af73647d66306ac2ed7e959
riturajkadamati <riturajkadamati@touch2success.com>
2020-02-12 18:20:26 +0530

    Added navigation

---
+++ AppModules/Navigation/Navigation.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import "react-native-gesture-handler";
+import { createStackNavigator } from "react-navigation-stack";
+import QRCode from "../Views/QRCode";
+import StartInspection from "../Views/StartInspection";
+import FinishInspection from "../Views/FinishInspection";
+import LocationPermission from "../Views/LocationsPermissions";
+export class Navigation extends Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+const AppNavigator = createStackNavigator({
+  Location: LocationPermission,
+  Inspection: StartInspection,
+  Finish: FinishInspection,
+  QRCode: QRCode
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+export default AppContainer;
--- AppModules/Views/QRCode.js
+++ AppModules/Views/QRCode.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 
-export default class navigation extends Component {
+export default class QRCode extends Component {
   render() {
     return (
       <View>
