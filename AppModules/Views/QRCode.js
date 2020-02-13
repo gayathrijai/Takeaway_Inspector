2d6a38202d8930c7d6bdf1a701d27385d32e8225
Dev <mohan.s@touch2success.com>
2020-02-13 05:57:46 +0000

    added qr

--- AppModules/Views/QRCode.js
+++ AppModules/Views/QRCode.js
@@ -5,7 +5,7 @@ export default class QRCode extends Component {
   render() {
     return (
       <View>
-        <Text> textInComponent </Text>
+        <Text> QR </Text>
       </View>
     );
   }
